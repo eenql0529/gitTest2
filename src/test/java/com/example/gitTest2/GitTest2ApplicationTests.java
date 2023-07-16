4af8035b6caea7c6974687d714d2ff36a412aca5
eenql0529 <eenql0529@daum.net>
2023-07-16 20:57:39 +0900

    “메모”

---
+++ src/test/java/com/example/gitTest2/GitTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitTest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
